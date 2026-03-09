4b695dbd57f7d1415c7761b5b1536673cede916c
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-03-09 07:42:22 +0000

    Add Custome Function to Sync Swimmer from Firebase

--- lib/custom_code/actions/index.dart
+++ lib/custom_code/actions/index.dart
@@ -1,1 +1,2 @@
 export 'batch_create_activities.dart' show batchCreateActivities;
+export 'update_swimmer_and_sync.dart' show updateSwimmerAndSync;
---
+++ lib/custom_code/actions/update_swimmer_and_sync.dart
@@ -0,0 +1,23 @@
+// Automatic FlutterFlow imports
+import '/backend/backend.dart';
+import '/backend/schema/structs/index.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'index.dart'; // Imports other custom actions
+import '/flutter_flow/custom_functions.dart'; // Imports custom functions
+import 'package:flutter/material.dart';
+// Begin custom action code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+Future updateSwimmerAndSync(
+  DocumentReference swimmerRef,
+  String newName,
+  String newGroup,
+  String newZone,
+) async {
+  // We leave this empty for now.
+  // This satisfies the "Declaration" requirement so we can Save.
+  return;
+}
+// Set your action name, define your arguments and return parameter,
+// and then add the boilerplate code using the green button on the right!
